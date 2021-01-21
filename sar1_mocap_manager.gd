24e04825fb77e284e304d742c38e8bcdb17c721c
Saracen <SaracenOne@gmail.com>
2021-01-21 13:40:47 +0000

    Add user preferences callback

--- sar1_mocap_manager.gd
+++ sar1_mocap_manager.gd
@@ -1,8 +1,16 @@
 extends Node
 tool
 
+const USER_PREFERENCES_SECTION_NAME = "mocap"
+
+var set_settings_value: FuncRef = FuncRef.new()
+var get_settings_value: FuncRef = FuncRef.new()
+var save_settings: FuncRef = FuncRef.new()
+
 const MOCAP_VERSION = 0
 const MOCAP_DIR = "mocap"
+
+var recording_enabled = false
 
 # 
 const MOCAP_EXT = ".mcp"
@@ -113,6 +121,37 @@ func start_recording(p_fps: int) -> MocapRecording:
 		mocap_recording.write_mocap_header(MOCAP_VERSION, p_fps)
 	
 	return mocap_recording
+	
+func set_settings_value(p_key: String, p_value) -> void:
+	if set_settings_value.is_valid():
+		set_settings_value.call_func(USER_PREFERENCES_SECTION_NAME, p_key, p_value)
+	
+func set_settings_values():
+	set_settings_value("recording_enabled", recording_enabled)
+	
+func get_settings_value(p_key: String, p_type: int, p_default):
+	if get_settings_value.is_valid():
+		return get_settings_value.call_func(USER_PREFERENCES_SECTION_NAME, p_key, p_type, p_default)
+	else:
+		return p_default
+	
+func is_quitting() -> void:
+	set_settings_values()
+	
+func get_settings_values() -> void:
+	recording_enabled = get_settings_value("recording_enabled", TYPE_BOOL, recording_enabled)
+	
+func assign_set_settings_value_funcref(p_instance: Object, p_function: String) -> void:
+	set_settings_value.set_instance(p_instance)
+	set_settings_value.set_function(p_function)
+	
+func assign_get_settings_value_funcref(p_instance: Object, p_function: String) -> void:
+	get_settings_value.set_instance(p_instance)
+	get_settings_value.set_function(p_function)
+	
+func assign_save_settings_funcref(p_instance: Object, p_function: String) -> void:
+	save_settings.set_instance(p_instance)
+	save_settings.set_function(p_function)
 	
 func _ready():
 	Directory.new().make_dir("user://mocap")
