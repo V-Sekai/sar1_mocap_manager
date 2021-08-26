f804fbaa665fa1f548290bc43c72f34927e4cd1e
Lyuma <xn.lyuma@gmail.com>
2021-08-26 04:31:40 -0700

    godot import plugin naming convention

--- sar1_mcp_importer.gd
+++ sar1_mcp_importer.gd
@@ -4,31 +4,31 @@ extends EditorImportPlugin
 const mocap_functions_const = preload("sar1_mocap_functions.gd")
 const mocap_constants_const = preload("sar1_mocap_constants.gd")
 
-func get_importer_name():
+func _get_importer_name():
 	return "mcp_importer"
 
-func get_visible_name():
+func _get_visible_name():
 	return "Mocap Data"
 
-func get_recognized_extensions():
+func _get_recognized_extensions():
 	return ["mcp"]
 
-func get_save_extension():
+func _get_save_extension():
 	return "scn"
 
-func get_resource_type():
+func _get_resource_type():
 	return "PackedScene"
 
-func get_preset_count():
+func _get_preset_count():
 	return 1
 
-func get_preset_name(i):
+func _get_preset_name(i):
 	return "Default"
 
-func get_import_options(i):
+func _get_import_options(i):
 	return []
 
-func import(source_file, save_path, options, platform_variants, gen_files):
+func _import(source_file, save_path, options, platform_variants, gen_files):
 	var mocap_recording = MocapRecording.new(source_file)
 	if mocap_recording.open_file_read() == OK:
 		mocap_recording.parse_file()
@@ -36,7 +36,7 @@ func import(source_file, save_path, options, platform_variants, gen_files):
 
 		var packed_scene: PackedScene = mocap_functions_const.create_packed_scene_for_mocap_recording(mocap_recording)
 		if packed_scene:
-			var filename: String = save_path + "." + get_save_extension()
+			var filename: String = save_path + "." + _get_save_extension()
 			ResourceSaver.save(filename, packed_scene)
 			return OK
 	else:
